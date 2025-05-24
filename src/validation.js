f2b5cc0d61c70cf6fb0c27a4e1337cacb5e242e8
Vilmer Fall <vilmer.fall@gmail.com>
2025-05-24 08:49:45 +0200

    refactor isProduct with Joi

--- src/validation.js
+++ src/validation.js
@@ -1,9 +1,18 @@
+import Joi from "joi";
+
 // Remember to use RED, GREEN, REFACTOR
 // 1. pick one test case in validation.test.js
 // 2. write the code, verify that the test is RED
 // 3. write code in this file so that the test case becomes GREEN
 // 4. refactor as neccessary before you move on to the next
 // 5. repeat
+
+const productSchema = Joi.object({
+	id: Joi.number().integer().min(0).required(),
+	name: Joi.string().min(1).required(),
+	price: Joi.number().positive().required()
+});
+
 
 function isCartItem(object) {
 	if(
@@ -18,15 +27,8 @@ function isCartItem(object) {
 }
 
 function isProduct(object) {
-	if(
-		typeof object !== "object" || object === null ||
-		typeof object.id !== "number" ||
-		typeof object.name !== "string" ||
-		typeof object.price !== "number" || object.price <= 0
-	) {
-		return false;
-	}
-	return true;
+	const { error } = productSchema.validate(object);
+	return !error;
 }
 
 
