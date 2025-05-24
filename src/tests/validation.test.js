28d990898356e38eed87fa85591210b67b3c9ec8
Vilmer Fall <vilmer.fall@gmail.com>
2025-05-24 09:17:56 +0200

    cartItem id test RED

--- src/tests/validation.test.js
+++ src/tests/validation.test.js
@@ -14,39 +14,11 @@ const exampleCartObject = {
 
 // Group tests using "describe"
 describe('Validation', () => {
-
-	// Använd en "test" eller "it" (de är synonymer) för varje testfall
-	/* Exempel på syntax:
-	test('beskriv testfallet', () => {
-		// här skriver du testkoden
-		// avsluta alltid med "expect"
-	})
-	*/
-
-
-	// ---------------------------------------------
-	// Följande testfall ska du implementera. Det är tillåtet att använda Joi. Gör i så fall ett schema för varje sorts objekt du vill kunna validera. Du får även ändra texten och du t.ex. vill skriva på svenska i stället för engelska.
-	// (Ta bort dessa kommentarer när du är klar)
-
 	// 1. it returns true for a valid cart object
 	// 2. it returns false for invalid cart objects
 
 	// 3. it returns true for a valid product
 	// 4. it returns false for invalid cart objects
-
-	test('it returns true for a valid cart object', () => {
-		expect(isCartItem(exampleCartObject)).toBe(true)
-	})
-	test("returns false for an invalid cart items", () => {
-		const invalidCartItem = {
-			id: 2001,
-			item: "Banana"
-		}
-		expect(isCartItem(invalidCartItem)).toBe(false)
-	})
-
-
-
 
 
 	test("it  returns true for valid product", () => {
@@ -60,4 +32,19 @@ describe('Validation', () => {
 		expect(isProduct(invalidProduct)).toBe(false)
 	})
 
+
+	describe("isCartItem", () => {
+		test("returns true for a valid cart item", () => {
+			expect(isCartItem(exampleCartObject)).toBe(true)
+		})
+		test("returns false for invalid cart item with invalid amount", () => {
+			const invalidCartItem = {...exampleCartObject, amount: -1 }
+			expect(isCartItem(invalidCartItem)).toBe(false)
+		})
+		test("returns false for invalid cart item with invalid id", () => {
+			const invalidCartItem = {...exampleCartObject }
+			// delete invalidCartItem.id
+			expect(isCartItem(invalidCartItem)).toBe(false)
+		})
+	})
 })
